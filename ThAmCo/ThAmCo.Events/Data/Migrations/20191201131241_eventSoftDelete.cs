41aacfac55d5dcc0f88a3ae045e1df51fba3d473
ChrisBurrellT7145969 <t7145969@live.tees.ac.uk>
2019-12-01 13:13:03 +0000

    SoftDelete migration

---
+++ ThAmCo/ThAmCo.Events/Data/Migrations/20191201131241_eventSoftDelete.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ThAmCo.Events.Data.Migrations
+{
+    public partial class eventSoftDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                schema: "thamco.events",
+                table: "Events",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                schema: "thamco.events",
+                table: "Events");
+        }
+    }
+}
