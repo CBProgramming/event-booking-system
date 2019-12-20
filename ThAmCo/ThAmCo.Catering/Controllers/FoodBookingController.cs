fca328ce33e7e5157bdab1efdcfd05bdbbbce314
ChrisBurrellT7145969 <t7145969@live.tees.ac.uk>
2019-12-20 19:12:58 +0000

    FoodBooking api controller started

---
+++ ThAmCo/ThAmCo.Catering/Controllers/FoodBookingController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ThAmCo.Catering.Data;
+
+namespace ThAmCo.Catering.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodBookingController : ControllerBase
+    {
+        private readonly MenusDbContext _context;
+
+        public FoodBookingController(MenusDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetReservation([FromBody] int eventId)
+        {
+            var booking = await _context.FoodBookings.FindAsync(eventId);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+            FoodBookingDto bookingDto= new FoodBookingDto(booking);
+            return Ok(bookingDto);
+        }
+    }
+}
---
+++ ThAmCo/ThAmCo.Catering/Models/FoodBookingDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThAmCo.Catering.Data
+{
+    public class FoodBookingDto
+    {
+
+        public FoodBookingDto(FoodBooking booking)
+        {
+            MenuId = booking.MenuId;
+            EventId = booking.EventId;
+        }
+
+        public int MenuId { get; set; }
+
+        public int EventId { get; set; }
+
+    }
+}
